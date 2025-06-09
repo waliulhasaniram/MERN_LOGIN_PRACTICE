5feb95da0e678b22cc834b45678140eb0500ea4c
Md. Waliul Hasan Iram <97869370+waliulhasaniram@users.noreply.github.com>
2025-06-10 04:01:23 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Home from "@components/Home"
 import Login from "@components/Login"
 import Logout from "@components/Logout"
-import Registration from "@components/registration"
+import Registration from "@components/Registration"
 
 function App() {
 
